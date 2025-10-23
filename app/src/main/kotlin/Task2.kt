c698f2ab30152406fcefb0750698c8707f841dbf
stasenko <alipsid@mail.ru>
2025-10-23 07:35:12 +0300

    Report task2

---
+++ app/src/main/kotlin/Task2.kt
@@ -0,0 +1,17 @@
+package org.example.app
+
+fun main() {
+
+    var numberOfOrders: Int = 75
+    val gratitude: String ="Благодарим вас за заказ!" //кириллица не отображается в консоли
+
+    println(numberOfOrders)
+    println(gratitude)
+
+    var numberOfWorkers = 2000
+//    println(numberOfWorkers)
+
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+
+}
