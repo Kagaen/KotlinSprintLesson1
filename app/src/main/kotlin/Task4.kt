6bd0b93298905da512bbbc7295c4b963ea6fe1f8
stasenko <alipsid@mail.ru>
2025-10-23 09:44:40 +0300

    Report task4

---
+++ app/src/main/kotlin/Task4.kt
@@ -0,0 +1,19 @@
+package org.example.app
+
+fun main(){
+
+    val length: Long = 408686000000L
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Short = 32700
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
