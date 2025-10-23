f63edf6db0d572ad7188ee707dde7f8cd036a176
stasenko <alipsid@mail.ru>
2025-10-23 17:19:04 +0300

    KSL-1-4 fix "apogee" value and data type

--- app/src/main/kotlin/KSL-1-4.kt
+++ app/src/main/kotlin/KSL-1-4.kt
@@ -7,7 +7,7 @@ fun main(){
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Short = 32700
+    val apogee: Int = 327000
 
     println(length)
     println(age)
