003286c53d97779c4207f9dcf49063f52ef9e72a
stasenko <alipsid@mail.ru>
2025-10-24 13:34:09 +0300

    KSL-2-1 report

---
+++ app/src/main/kotlin/KSL-2-1.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+
+    val studPoint1 = 3.0
+    val studPoint2 = 4.0
+    val studPoint3 = 3.0
+    val studPoint4 = 5.0
+
+    val averageMark = (studPoint1 + studPoint2 + studPoint3 + studPoint4) / 4
+    println(String.format("%.2f", averageMark))
+
+}
