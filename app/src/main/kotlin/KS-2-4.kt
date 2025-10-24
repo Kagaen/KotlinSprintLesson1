177be54388910b8e449bb7e3be450c36316d1d08
stasenko <alipsid@mail.ru>
2025-10-24 16:11:08 +0300

    KS-2-4 report

--- app/src/main/kotlin/KS-2-4.kt
+++ app/src/main/kotlin/KS-2-4.kt
@@ -6,7 +6,7 @@ fun main() {
     val ironOreValue = 11
     val buff = 1.2
 
-    println("Bonus krystal ore - ${"%.0f".format(krystalOreValue * buff)} ")
-    println("Bonus iron ore - ${"%.0f".format(ironOreValue * buff)} ")
+    println("Bonus krystal ore - ${"%.0f".format(krystalOreValue * buff - krystalOreValue)} ")
+    println("Bonus iron ore - ${"%.0f".format(ironOreValue * buff - ironOreValue)} ")
 
 }
