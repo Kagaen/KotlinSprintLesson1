b8480512518decf5077d431958afb1ce0daedf20
stasenko <alipsid@mail.ru>
2025-10-24 14:07:39 +0300

    KS-2-2 fix

--- app/src/main/kotlin/KS-2-2.kt
+++ app/src/main/kotlin/KS-2-2.kt
@@ -2,7 +2,7 @@ package org.example
 
 import java.util.Locale
 
-fun main(){
+fun main() {
 
     val employeeCount = 50
     val employeeSalary = 30000
@@ -10,11 +10,11 @@ fun main(){
     val internSalary = 20000
 
     val employeeExpenses = employeeSalary * employeeCount
-    val allExpenses = employeeSalary * employeeCount + internSalary * internCount
+    val allExpenses = employeeExpenses + internSalary * internCount
     val averageSalary = allExpenses / (employeeCount + internCount)
 
-    println(String.format(Locale.UK,"%,d", employeeExpenses))
-    println(String.format(Locale.UK,"%,d",allExpenses))
-    println(String.format(Locale.UK,"%,d",averageSalary))
+    println(String.format(Locale.UK, "%,d", employeeExpenses))
+    println(String.format(Locale.UK, "%,d", allExpenses))
+    println(String.format(Locale.UK, "%,d", averageSalary))
 
 }
