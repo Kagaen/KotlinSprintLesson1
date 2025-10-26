001e55383b0f3ad6506f5eb001690859dc284dcf
stasenko <alipsid@mail.ru>
2025-10-26 12:01:26 +0300

    save

--- app/src/main/kotlin/KS-1-1.kt
+++ app/src/main/kotlin/KS-1-1.kt
@@ -2,7 +2,7 @@ package org.example.app
 
 fun main() {
 
-    var numberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val gratitude: String ="Благодарим вас за заказ!"
 //сдача задания
 
