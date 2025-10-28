3aa9ab715d678d8dd2e9190222f4ca6fb8df7a21
stasenko <alipsid@mail.ru>
2025-10-28 19:56:45 +0300

    add const

--- app/src/main/kotlin/KS-5-6.kt
+++ app/src/main/kotlin/KS-5-6.kt
@@ -1,20 +1,25 @@
 import java.util.Locale
 import kotlin.math.pow
 
+const val KM_EDGE = 100
+const val UNDERWEIGHT = 18.5
+const val NORMAL_WEIGHT = 25
+const val OVERWEIGHT = 30
+
 fun main() {
 
     print("Your height in cm: ")
-    val height = readln().toDouble() / 100
+    val height = readln().toDouble() / KM_EDGE
     print("Your weight in kg: ")
     val weight = readln().toDouble()
     val bmi = weight / height.pow(2)
     val niceBmi = String.format(Locale.UK, "%.2f", bmi)
 
     when {
-        bmi < 18.5 -> println("BMI = $niceBmi, underweight ")
-        (bmi >= 18.5) && (bmi < 25) -> println("BMI = $niceBmi, normal body weight")
-        (bmi >= 25) && (bmi < 30) -> println("BMI = $niceBmi, overweight")
-        bmi >= 30 -> println("BMI = $niceBmi, obese")
+        bmi < UNDERWEIGHT -> println("BMI = $niceBmi, underweight ")
+        bmi < NORMAL_WEIGHT -> println("BMI = $niceBmi, normal body weight")
+        bmi < OVERWEIGHT -> println("BMI = $niceBmi, overweight")
+        bmi >= OVERWEIGHT -> println("BMI = $niceBmi, obese")
     }
 
 }
