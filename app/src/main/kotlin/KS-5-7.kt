09056f0ccff02b65b164cc89a137915f27e6ce61
stasenko <alipsid@mail.ru>
2025-10-28 15:36:41 +0300

    KS-5-7 report

---
+++ app/src/main/kotlin/KS-5-7.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    print("Distance in km: ")
+    val distance = readln().toDouble()
+    print("Consumption per 100km: ")
+    val consumption = readln().toDouble()
+    print("Price per liter: ")
+    val price = readln().toDouble()
+
+    val literCount = (distance * consumption) / 100
+    val cost = literCount * price
+
+    println("Cost of the travel: ${"%.2f".format(cost)}\n" + "Need liters: ${"%.2f".format(literCount)}")
+
+}
