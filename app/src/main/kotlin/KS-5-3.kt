c32b64329032fa464e1e252efcfdd05b68cdabf6
stasenko <alipsid@mail.ru>
2025-10-28 12:47:31 +0300

    KS-5-3 fix

--- app/src/main/kotlin/KS-5-3.kt
+++ app/src/main/kotlin/KS-5-3.kt
@@ -1,33 +1,20 @@
-import kotlin.random.Random
-
 fun main() {
 
-    val firstLotteryNumber = Random.nextInt(0, 42)
-    val secondLotteryNumber = Random.nextInt(0, 42)
-    println("Try to guess the numbers from 0 to 42 ($firstLotteryNumber, $secondLotteryNumber)")
+    val firstNumber = (0..42).random()
+    val secondNumber = (0..42).random()
+    println("Try to guess the two numbers from 0 to 42 ")
 
-    val userFirstNumber = readln().toIntOrNull()
-    val userSecondNumber = readln().toIntOrNull()
+    val userFirstNum = readln().toIntOrNull()
+    val userSecondNum = readln().toIntOrNull()
 
-    if (userFirstNumber == firstLotteryNumber) {
-        if (userSecondNumber == secondLotteryNumber) {
-            println("Congratulations! You've won the grand prize!")
-        } else {
-            println("You've won a consolation prize!")
-        }
-    } else {
-        if (userFirstNumber == secondLotteryNumber) {
-            if (userSecondNumber == firstLotteryNumber) {
-                println("Congratulations! You've won the grand prize!")
-            } else {
-                println("You've won a consolation prize!")
-            }
-        } else {
-            if (userSecondNumber == firstLotteryNumber) {
-                println("You've won a consolation prize!")
-            } else {
-                println("Bad Luck!")
-            }
-        }
-    }
+    println(
+        if ((userFirstNum == firstNumber) && (userSecondNum == secondNumber) || (userFirstNum == secondNumber) && (userSecondNum == firstNumber)) {
+            "Congratulations! You've won the grand prize!"
+        } else if ((userFirstNum != firstNumber) && (userSecondNum != secondNumber) && (userFirstNum != secondNumber) && (userSecondNum != firstNumber)) {
+            "Bad Luck!"
+        } else "You've won a consolation prize!"
+    )
+
+    println("$firstNumber, $secondNumber")
+
 }
