22fff2bf9d87c5029d88701b7ddbbd9b8e87394a
stasenko <alipsid@mail.ru>
2025-11-01 16:20:12 +0300

    KS-6-2 fix

--- app/src/main/kotlin/KS-6-2-Timer.kt
+++ app/src/main/kotlin/KS-6-2-Timer.kt
@@ -3,9 +3,12 @@ import kotlin.math.roundToLong
 fun main() {
 
     val rounding = 1000
+
     print("Set timer in sec: ")
-    val timer = readln().format("%.2f").toDouble() * rounding
-    Thread.sleep(timer.roundToLong())
-    println("Passed ${"%.2f".format(timer / rounding)} seconds")
+    val counter = readln().toDouble()
+
+    val timer = (counter * rounding).roundToLong()
+    Thread.sleep(timer)
+    println("Passed ${"%.2f".format(counter)} seconds")
 
 }
