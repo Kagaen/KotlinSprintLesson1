64d0b9ffac958e363ab02132246a185eed9b00ba
stasenko <alipsid@mail.ru>
2025-11-03 19:35:35 +0300

    KS-9-1 report

---
+++ app/src/main/kotlin/KS-9-1-List.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+    val recipe = listOf("water", "meat", "potato", "carrot", "herbs")
+    println("The recipe contains the following ingredients:")
+    recipe.forEach { println(it) }
+
+}
