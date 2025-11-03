168d385635be6e1b53ef0dfb3d1caaf584bf8ee6
stasenko <alipsid@mail.ru>
2025-11-03 20:26:11 +0300

    KS-9-3 report

--- app/src/main/kotlin/KS-9-3.kt
+++ app/src/main/kotlin/KS-9-3.kt
@@ -5,7 +5,7 @@ fun main() {
     print("Portions count: ")
     val count = readln().toInt()
     val order = omeletRecipe.map { it * count }
-    println("For ${count} omelettes need:\n" +
+    println("For $count omelettes need:\n" +
             "eggs - ${order[0]}, milk - ${order[1]} ml, butter - ${order[2]} g")
 
 }
