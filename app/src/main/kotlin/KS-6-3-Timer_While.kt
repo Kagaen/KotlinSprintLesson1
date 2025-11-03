e78af822e9388fe1ce0303e134bf5b2f6e5cb3b7
stasenko <alipsid@mail.ru>
2025-11-03 14:40:05 +0300

    KS-6-3 убрал дробную часть

--- app/src/main/kotlin/KS-6-3-Timer_While.kt
+++ app/src/main/kotlin/KS-6-3-Timer_While.kt
@@ -1,18 +1,11 @@
-import kotlin.math.roundToLong
-
 fun main() {
 
-    val msInSec = 1000
     print("Set timer in sec: ")
-    var counter = readln().toDouble()
-    var timer = (counter * msInSec).roundToLong()
+    var counter = readln().toInt()
 
     while (counter > 0) {
-        println("${"%.2f".format(counter--)} left")
-        if (timer < msInSec) {
-            Thread.sleep(timer)
-        } else Thread.sleep(1000)
-        timer -= 1000
+        println("${counter--} left")
+        Thread.sleep(1000)
     }
     println("Time out")
 
