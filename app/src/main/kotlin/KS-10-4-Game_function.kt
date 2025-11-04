0b35a64b09ba92262784fde5db98115ce01683f3
stasenko <alipsid@mail.ru>
2025-11-04 22:58:01 +0300

    KS-10-4 moved 'do-while' to 'main'

--- app/src/main/kotlin/KS-10-4-Game_function.kt
+++ app/src/main/kotlin/KS-10-4-Game_function.kt
@@ -1,28 +1,27 @@
 fun main() {
-    game()
+
+    var wins = 0
+    do {
+        val result = game()
+        if (result == "Humanity win") wins++
+        println("$result\n" +
+                "Do you want to roll the dice again? Enter “Yes” or “No”")
+        val answer = readln().trim().lowercase()
+        if (answer == "no") println("Your wins: $wins")
+    } while (answer == "yes")
+
 }
 
 fun roll(): Int = (1..6).random()
 
-fun game() {
-    var humanWins = 0
-    do {
-        val machineRoll = roll()
-        println("Machine dice: $machineRoll")
-        val humanRoll = roll()
-        println("Human dice: $humanRoll")
-        when {
-            (machineRoll > humanRoll) -> println("Machine win")
-            (machineRoll < humanRoll) -> {
-                println("Humanity win")
-                humanWins++
-            }
-
-            else -> println("Draw")
-        }
-        println("Do you want to roll the dice again? Enter “Yes” or “No”")
-        val answer = readln().trim()
-        if (answer.equals("no", ignoreCase = true)) println("Your wins: $humanWins")
-    } while (answer.equals("yes", ignoreCase = true))
+fun game(): String {
+    val machineRoll = roll()
+    println("Machine dice: $machineRoll")
+    val humanRoll = roll()
+    println("Human dice: $humanRoll")
+    return when {
+        machineRoll > humanRoll -> "Machine win"
+        machineRoll < humanRoll -> "Humanity win"
+        else -> "Draw"
+    }
 }
-
