db88985562d96c3120349885940f7f2719603a57
stasenko <alipsid@mail.ru>
2025-11-07 03:52:38 +0300

    KS-11-5 add printThread

--- app/src/main/kotlin/KS-11-5-Forum.kt
+++ app/src/main/kotlin/KS-11-5-Forum.kt
@@ -1,12 +1,11 @@
 class Forum() {
-    val memberId: MutableSet<Int> = mutableSetOf()
-    val memberName: MutableList<String> = mutableListOf()
-    val memberChat: MutableList<Pair<Int, String>> = mutableListOf()
-
+    val members: MutableMap<Int, String> = linkedMapOf()
+    val forumChat: MutableList<Pair<Int, String>> = mutableListOf()
 
     fun printTread() {
-        for ((id, message) in memberChat) {
-
+        for ((id, message) in forumChat) {
+            val memberName = members[id]
+            if (memberName != null) println("$memberName: $message")
         }
     }
 }
@@ -16,14 +15,10 @@ class ForumMember() {
         var userId = 0
         var userName = ""
         fun createNewUser(name: String): Builder {
-            userId = if (forum.memberId.isEmpty()) {
-                0
-            } else {
-                forum.memberId.last() + 1
-            }
+            userId = forum.members.keys.maxOrNull() ?: 0
+            userId +=  1
             userName = name
-            forum.memberId.add(userId)
-            forum.memberName.add(userName)
+            forum.members[userId] = userName
             return this
         }
     }
@@ -32,10 +27,12 @@ class ForumMember() {
 class ForumMessage() {
     class Builder(val forum: Forum) {
         var authorId = 0
-        var message = ""
+        var authorMessage = ""
         fun createMessage(id: Int, message: String) {
-            if (forum.memberId.contains(id)) {
-                forum.memberChat.add(id to message)
+            if (forum.members.contains(id)) {
+                authorId = id
+                authorMessage = message
+                forum.forumChat.add(authorId to authorMessage)
             }
         }
     }
@@ -46,13 +43,11 @@ fun main() {
     val greatForum = Forum()
     val firsMember = ForumMember.Builder(greatForum).createNewUser("The chosen one")
     val secondMember = ForumMember.Builder(greatForum).createNewUser("The chosen two")
-    println(greatForum.memberId)
-    println(greatForum.memberName)
 
-    val message1 = ForumMessage.Builder(greatForum).createMessage(0, "Hi!")
+    val message1 = ForumMessage.Builder(greatForum).createMessage(2, "Oh, Hello!")
     val message2 = ForumMessage.Builder(greatForum).createMessage(1, "Здарова, коль не шутишь")
-    val message3 = ForumMessage.Builder(greatForum).createMessage(0, "Где Я?")
-    println(greatForum.memberChat)
+    val message3 = ForumMessage.Builder(greatForum).createMessage(3, "Где Я?")
+
     greatForum.printTread()
 
 
