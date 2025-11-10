45b2fda7f907512f969f0b59a34203da27447a4c
stasenko <alipsid@mail.ru>
2025-11-10 21:31:28 +0300

    KS-13-5 report

---
+++ app/src/main/kotlin/KS-13-5.kt
@@ -0,0 +1,26 @@
+data class Contact2(
+    var number: Long?,
+    var name: String?,
+    var company: String? = null,
+) {
+    fun newContact(): Contact2 {
+        print("Номер: ")
+        try {
+            val number = readln().toLong()
+        } catch (e: Exception) {
+            println("Ошибка: ${e.message}")
+        }
+        print("Имя: ")
+        val name: String = readln()
+        print("Компания: ")
+        val company = readln()
+        return Contact2(number, name, company)
+    }
+}
+
+fun main() {
+
+    val contact = Contact2(null, null, null)
+    contact.newContact()
+
+}
