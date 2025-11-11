2c12043f5e3967668859f20589f87efc6e8e430f
stasenko <alipsid@mail.ru>
2025-11-11 15:19:20 +0300

    KS-14-2 report

---
+++ app/src/main/kotlin/KS-14-2.kt
@@ -0,0 +1,51 @@
+fun main() {
+
+    val goodWay = Liner1()
+    val unstoppable = Icebreaker1()
+    val magdalena = Cargo1()
+
+    goodWay.showShipProp()
+    goodWay.gangway()
+    unstoppable.showShipProp()
+    unstoppable.openAftGate()
+    magdalena.showShipProp()
+    magdalena.craneActivate()
+
+}
+
+open class Liner1(
+    val speed: Int = 100,
+    val capacity: Int = 1000,
+    val tonnage: Int = 200,
+    val breakIceAbility: Boolean = false,
+) {
+    fun showShipProp() {
+        println("$speed\n$capacity\n$tonnage\n$breakIceAbility")
+    }
+
+    fun gangway() {
+        println("Выдвигаем трап со шкафута")
+    }
+
+}
+
+class Icebreaker1(
+    speed: Int = 50,
+    capacity: Int = 100,
+    breakIceAbility: Boolean = true,
+) : Liner1(speed, capacity, tonnage = 400, breakIceAbility) {
+    fun openAftGate() {
+        println("Кормовые ворота открыты")
+    }
+
+}
+
+class Cargo1(
+    speed: Int = 70,
+    capacity: Int = 50,
+) : Liner1(speed, capacity, tonnage = 2000) {
+    fun craneActivate() {
+        println("Погрузочный кран активирован")
+    }
+
+}
