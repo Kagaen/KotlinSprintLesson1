760639db0eae6903828a8a6fad83dcf95df11b93
stasenko <alipsid@mail.ru>
2025-11-14 01:44:02 +0300

    KS-15-3 report

---
+++ app/src/main/kotlin/KS-15-3.kt
@@ -0,0 +1,46 @@
+fun main() {
+
+    val user1 = UserKS153(1, "Potato")
+    val admin1 = AdminKS153(2, "KS")
+    user1.sendMessage()
+    admin1.deleteMessage()
+    admin1.deleteUser()
+}
+
+abstract class UsersAndAdmins() {
+    abstract val id: Int
+    abstract val name: String
+}
+
+class AdminKS153(
+    override val id: Int,
+    override val name: String,
+) : UsersAndAdmins(), AdminMoves {
+
+    override fun deleteMessage() {
+        println("Администратор удалил сообщение")
+    }
+
+    override fun deleteUser() {
+        println("Пользователь удалён администрацией")
+    }
+}
+
+class UserKS153(
+    override val id: Int,
+    override val name: String,
+) : UsersAndAdmins(), UserMoves {
+
+    override fun sendMessage() {
+        println("$name отправил сообщение")
+    }
+}
+
+interface UserMoves {
+    fun sendMessage()
+}
+
+interface AdminMoves {
+    fun deleteMessage()
+    fun deleteUser()
+}
