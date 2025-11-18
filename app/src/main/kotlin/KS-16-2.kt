6ac5e0235a70e9ffa17abdda5491060fa4adba13
stasenko <alipsid@mail.ru>
2025-11-18 10:40:25 +0300

    KS-16-2 report

--- app/src/main/kotlin/KS-16-2.kt
+++ app/src/main/kotlin/KS-16-2.kt
@@ -8,6 +8,6 @@ fun main() {
 
 class CircleKS162(private val rad: Int) {
     private val pi = 3.14
-    fun circleArea() = println("Площадь: ${pi * rad * rad}")
+    fun circleArea() = println("Площадь: ${"%.2f".format(pi * rad * rad)}")
     fun circleLength() = println("Длина окружности: ${pi * rad * 2}")
 }
