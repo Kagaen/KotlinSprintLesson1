f94aff3e71619ab4d6fa56f2f04fb312c4b4e5ce
stasenko <alipsid@mail.ru>
2025-12-15 18:55:30 +0300

    KS-19-3 fix

--- app/src/main/kotlin/KS-19-3.kt
+++ app/src/main/kotlin/KS-19-3.kt
@@ -1,22 +1,24 @@
+import Action.CLEAR_ASTEROID
+import Action.LAND
+import Action.TAKE_OFF
+
 enum class Action {
     TAKE_OFF,
     LAND,
-    CLEAR_ASTEROID;
+    CLEAR_ASTEROID,
+}
 
-    fun action(): String = when (this) {
-        TAKE_OFF -> "Взлёт"
-        LAND -> "Посадка"// TODO добавить выбор места посадки
+private class SpaceShip {
+    fun execute(move: Action) = when (move) {
+        TAKE_OFF -> println("Взлёт")
+        LAND -> println("Посадка")// TODO добавить выбор места посадки
         CLEAR_ASTEROID -> TODO()
     }
 }
 
-private class SpaceShip {
-    fun execute(move: Action) = println(move.action())
-}
-
 fun main() {
     val ship = SpaceShip()
-    ship.execute(Action.TAKE_OFF)
-    ship.execute(Action.LAND)
-    ship.execute(Action.CLEAR_ASTEROID)
+    ship.execute(TAKE_OFF)
+    ship.execute(LAND)
+    ship.execute(CLEAR_ASTEROID)
 }
