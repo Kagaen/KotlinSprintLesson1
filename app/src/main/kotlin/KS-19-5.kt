5b1e3b112b9e5fcc2e4da3060b4cd5fe591764f8
stasenko <alipsid@mail.ru>
2025-12-18 21:17:08 +0300

    KS-19-5 fix

--- app/src/main/kotlin/KS-19-5.kt
+++ app/src/main/kotlin/KS-19-5.kt
@@ -3,6 +3,8 @@ enum class Gender(val designation: String) {
     WOMAN("жен"),
 }
 
+private const val ERROR_FORMAT = "Неверный формат, попробуйте снова"
+
 private class Person(
     val name: String,
     val gender: Gender,
@@ -10,9 +12,9 @@ private class Person(
 
 private fun readGender(input: String): Gender? {
 
-    val gender = Gender.entries.find { it.designation == input }
+    val gender = Gender.entries.firstOrNull { it.designation.equals(input, ignoreCase = true) }
     if (gender == null) {
-        println("Неверный формат, попробуйте снова")
+        println(ERROR_FORMAT)
         return null
     }
     return gender
@@ -25,19 +27,18 @@ fun main() {
     println("Формат ввода: [Имя], [пол: \"${Gender.entries.joinToString("\" или \"") { it.designation }}\"]")
 
     while (fileCabinet.size < 5) {
-        val userInput = readlnOrNull()
-        if (userInput == null) {
-            println("Неверный формат, попробуйте снова")
-            continue
-        } else if (!userInput.contains(',')) {
-            println("Неверный формат, попробуйте снова")
-            continue
-        }
+        val userInput = readln()
         val input = userInput.split(",")
-        val name = input[0].trim()
-        val gender = readGender(input[1].trim()) ?: continue
-        fileCabinet.add(Person(name, gender))
-        println("$name добавлен в картотеку")
+        if (input.size == 2) {
+            val name = input[0].trim()
+            if (name == "") {
+                println(ERROR_FORMAT)
+                continue
+            }
+            val gender = readGender(input[1].trim()) ?: continue
+            fileCabinet.add(Person(name, gender))
+            println("$name добавлен в картотеку")
+        } else println(ERROR_FORMAT)
     }
     println()
     fileCabinet.forEach { println("Имя: ${it.name}; Пол: ${it.gender.designation}.") }
