9d6906f77f75b9f197b38bffb39ec0ef9e6d6559
stasenko <alipsid@mail.ru>
2026-02-14 19:37:30 +0300

    KS-20-4 fix

--- app/src/main/kotlin/KS-20-4.kt
+++ app/src/main/kotlin/KS-20-4.kt
@@ -1,9 +1,11 @@
 fun main() {
 
-    val listOfStrings = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven")
+    val listOfStrings =
+        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven")
 
-    listOfStrings.mapIndexed { index, str: String ->
-        println(if (index % 2 == 0) "press $str" else str)
+    val listOfLambdas: List<() -> String> = listOfStrings.map { str: String -> { "Нажат элемент \"$str\"" } }
+
+    listOfLambdas.mapIndexed { index: Int, lambda: () -> String ->
+        if (index % 2 == 0) println(lambda())
     }
-
 }
